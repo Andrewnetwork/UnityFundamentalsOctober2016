70362820e12e93ce03f60eb5659a60e09606c469
andrewribeiro <andrewnetwork@gmail.com>
2016-10-12 18:28:52 -0400

    v3

---
+++ OctoberPresentation/Assets/BuildWallDetect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildWallDetect : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Brick")
+		{
+			GameObject go = GameObject.Find("WallGen");
+			WallGen other = (WallGen) go.GetComponent(typeof(WallGen));
+
+			other.buildWall ();
+
+			AudioSource audio = GetComponent<AudioSource>();
+
+			audio.Play();
+
+
+		}
+	}
+}
